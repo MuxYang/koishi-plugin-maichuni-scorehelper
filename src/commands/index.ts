eece9294da2fdcd43d2413dbefa2e0210308d5c1
MuxYang <3489892672@qq.com>
2026-02-23 16:57:14 +0800

    Keep only fish-login-page in page branch

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Commands barrel export
- */
-export { registerMaimaiCommands } from './maimai'
-export { registerChunithmCommands } from './chunithm'
-export { registerFishCommands } from './fish'
-export { registerLxCommands } from './lx'
